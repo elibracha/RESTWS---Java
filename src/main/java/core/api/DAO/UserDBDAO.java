d99079962a416a9a2b8b299196bb64fbad89af10
eli brahca <elibracha45@gmail.com>
2018-04-11 03:09:05 +0300

    update

--- src/main/java/core/api/DAO/UserDBDAO.java
+++ src/main/java/core/api/DAO/UserDBDAO.java
@@ -32,6 +32,7 @@ public class UserDBDAO implements UserDAO{
 
 	@Override
 	public UserDTO getUserByName(String username) {
+
 		UserDTO userDTO = null;
 		
 		CriteriaBuilder cb = session.getCriteriaBuilder();
